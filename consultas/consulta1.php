081bec400c97890e4567e2414e9f04b815e0a2d2
Carlos Valencia <caenvagi016@gmail.com>
2025-04-04 22:17:21 -0500

    eliminando archivos no necesarios 	deleted:    assets/img/logofact.png 	deleted:    consultas/consulta1.php 	modified:   index.php

--- consultas/consulta1.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../conexion/conexion.php';
-$sql = "SELECT * FROM usuarios ";
-$stmt = $pdo->prepare($sql);
-//$stmt->execute(['email' => $email]);
-$result = $stmt->fetch();
-echo $result;
--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if ($_POST) {
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
 
 	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-	<script src="../js/scripts.js"></script>
+	
 
 	<!-- CSS  -->
 	<link href="css/estilos.css" type="text/css" rel="stylesheet" media="screen,projection" />
@@ -148,7 +148,7 @@ if ($_POST) {
 				?>
 		</div>
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-			<script src="js/scripts.js"></script>
+			
 	</main>
 </body>
 </html>
