f8699d855e53051f1423d992be3f8cc0eb440ecd
Carlos Valencia <caenvagi016@gmail.com>
2025-04-18 09:18:14 -0500

    CARGAR bootstrap

--- logs/head.php
+++ logs/head.php
@@ -12,7 +12,7 @@
     <link rel='stylesheet' type='text/css' href='../css/styles.css'>
     <link rel='stylesheet' type='text/css' href='../css/estilos.css'>
 
-    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>	
+    <script src="../modulos/jquery-3.7.1/jquery-3.7.1.js"></script>	
 
     <?php date_default_timezone_set('America/New_York'); ?>
     
@@ -24,20 +24,16 @@
     <!-- include the script -->
     <script src="../modulos/alertifyjs/alertify.min.js"></script>
     <script src="../js/mensajes.js"></script>
-    <script src="../js/scripts.js"></script>
-
-    	 
+    <script src="../js/scripts.js"></script>   	 
 	
     
-    <!-- Bootstrap CSS v5.0.2 -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap CSS-->
+    <script src="../modulos/bootstrap-5.3.5-dist/js/bootstrap.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../modulos/bootstrap-5.3.5-dist/css/bootstrap.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="../modulos/icons-1.11.3/font/bootstrap-icons.css">
+
     
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
     
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     
-    <!-- google icons-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>    
-</head>
+    
--- usuarios/usuarios_lista.php
+++ usuarios/usuarios_lista.php
@@ -3,33 +3,33 @@ session_start();
 require_once "../conexion/conexion.php";
 
 // CONSULTA USUARIOS EN TABLA USUARIOS    
-    try {
-        // Verifica que $pdo esté definido correctamente
+try {
+    // Verifica que $pdo esté definido correctamente
 
-        // Preparar la consulta
-        $stmt = $pdo->prepare("  SELECT id,nombre,avatar,activo,tipo_cargo,usuario,cargo_nombre
+    // Preparar la consulta
+    $stmt = $pdo->prepare("  SELECT id,nombre,avatar,activo,tipo_cargo,usuario,cargo_nombre
                                         FROM usuarios as US
                                         INNER JOIN tipo_cargo as TC ON TC.id_cargo = US.tipo_cargo
                                         ORDER BY activo DESC");
 
-        // Ejecutar la consulta
-        $stmt->execute();
+    // Ejecutar la consulta
+    $stmt->execute();
 
-        // Obtener resultados
-        $usuarios_activos = $stmt->fetchAll();
+    // Obtener resultados
+    $usuarios_activos = $stmt->fetchAll();
 
-        // Validar si hay datos
-        if (!empty($usuarios_activos)) {
-            // foreach ($usuarios_activos as $usuario) {
-            //     echo "ID: " . htmlspecialchars($usuario['id']) . " - Nombre: " . htmlspecialchars($usuario['nombre']) . "<br>";
-            // }
-        } else {
-            echo "No se encontraron usuarios.";
-        }
-    } catch (PDOException $e) {
-        error_log("Error en la consulta: " . $e->getMessage());
-        echo "Error al consultar los usuarios.";
+    // Validar si hay datos
+    if (!empty($usuarios_activos)) {
+        // foreach ($usuarios_activos as $usuario) {
+        //     echo "ID: " . htmlspecialchars($usuario['id']) . " - Nombre: " . htmlspecialchars($usuario['nombre']) . "<br>";
+        // }
+    } else {
+        echo "No se encontraron usuarios.";
     }
+} catch (PDOException $e) {
+    error_log("Error en la consulta: " . $e->getMessage());
+    echo "Error al consultar los usuarios.";
+}
 
 
 ?>
@@ -39,6 +39,9 @@ require_once "../conexion/conexion.php";
 
 <head>
     <?php require '../logs/head.php'; ?>
+    <!-- DataTable-->
+    <script src="../modulos/DataTables/datatables.js"></script>
+    <link rel="stylesheet" href="../modulos/DataTables/datatables.css">
 </head>
 
 <body>
@@ -47,97 +50,156 @@ require_once "../conexion/conexion.php";
         <main>
             <!-- navegacion horizontal -->
             <ul class="nav nav-tabs mt-3">
-                    <li class="nav-item">
-                        <a class="nav-link" href="usuarios_nuevos.php">Crear Usuario</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="usuarios_lista.php">Listado</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="mensualidades_list.php">Edicion</a>
-                    </li>
-                    <!-- <li class="nav-item">
+                <li class="nav-item">
+                    <a class="nav-link" href="usuarios_nuevos.php">Crear Usuario</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="usuarios_lista.php">Listado</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="mensualidades_list.php">Edicion</a>
+                </li>
+                <!-- <li class="nav-item">
                         <a class="nav-link" href="mensualidades_list.php">Listado de mensualidades</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="mensualidades_ven.php">Vencimiento</a> 
                     </li>-->
-                    <!-- <li class="nav-item">
+                <!-- <li class="nav-item">
                             <a class="nav-link disabled">Disabled</a>
                         </li> -->
-                </ul>
+            </ul>
             <!-- navegacion horizontal -->
             <!-- tabla empleados -->
-                <div class="justify-content-between m-0 col col-10 col-sm-10 col-md-12">
-                    <div class="card m-2">
+            <div class="justify-content-between m-0 col col-10 col-sm-10 col-md-12">
+                <div class="card m-2">
                     <div class="card-header">
                         Empleados registrados:
                     </div>
                     <div class="card-body">
-                    <table id="tabla_empleados" class="table table table-sm table-borderless table-hover mt-3 table text-center table align-middle">
-                        <thead>
-                            <tr>
-                                <th align="center">NOMBRE</th>
-                                <th align="center">AVATAR</th>
-                                <th align="center">CARGO</th>
-                                <th align="center">USER</th>
-                                <th align="center">ACTIVO</th>
-                                <th align="center">EDITAR</th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                                <?php foreach ($usuarios_activos as $usuario): ?>
-                                
+                        <table id="tabla_empleados" class="table table table-sm table-borderless table-hover mt-3 table text-center table align-middle">
+                            <thead>
                                 <tr>
-                                    <td align="center"><a href=""><?= htmlspecialchars($usuario['nombre']) ?></a></td>
-                                                                            
-                                    <td><?php
-                                        if (isset($usuario['activo']) && $usuario['activo'] == "1") {
-                                            echo "<img class='avatar4' src='{$usuario['avatar']}'>";
-                                        } else {
-                                            echo "<img class='avatar5' src='{$usuario['avatar']}'>";
-                                        }
-                                        ?>
-                                    </td>
-                                    <td><?= htmlspecialchars($usuario['cargo_nombre']) ?></td>
-                                    <td><?= htmlspecialchars($usuario['usuario']) ?></td>
-                                    
-                                    <?php
+                                    <th align="center">NOMBRE</th>
+                                    <th align="center">AVATAR</th>
+                                    <th align="center">CARGO</th>
+                                    <th align="center">USER</th>
+                                    <th align="center">ACTIVO</th>
+                                    <th align="center">EDITAR</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($usuarios_activos as $usuario): ?>
+
+                                    <tr>
+                                        <td align="center"><a href=""><?= htmlspecialchars($usuario['nombre']) ?></a></td>
+
+                                        <td><?php
+                                            if (isset($usuario['activo']) && $usuario['activo'] == "1") {
+                                                echo "<img class='avatar4' src='{$usuario['avatar']}'>";
+                                            } else {
+                                                echo "<img class='avatar5' src='{$usuario['avatar']}'>";
+                                            }
+                                            ?>
+                                        </td>
+                                        <td><?= htmlspecialchars($usuario['cargo_nombre']) ?></td>
+                                        <td><?= htmlspecialchars($usuario['usuario']) ?></td>
+
+                                        <?php
                                         if (isset($usuario['activo']) && $usuario['activo'] == "1") {
                                             echo "<td data-activo='1'> <h3 style=color:green><i class='bi bi-person-check-fill'></i></h3> </td>";
                                         } else {
                                             echo "<td data-activo='0'> <h3 style=color:grey><i class='bi bi-person-x-fill'></i></h3> </td>";
                                         }
                                         ?>
-                                    <td align="center"><a href="usuarios_editar.php?id=<?= htmlspecialchars($usuario['id']) ?>" title="Editar" class="btn btn-outline-success btn-xs">
-                                    <i class="bi bi-pencil-square"></i></a></td>                               
-                                </tr>
+                                        <td align="center"><a href="usuarios_editar.php?id=<?= htmlspecialchars($usuario['id']) ?>" title="Editar" class="btn btn-outline-success btn-xs">
+                                                <i class="bi bi-pencil-square"></i></a></td>
+                                    </tr>
                                 <?php endforeach; ?>
-                        </tbody>
-                        <tfoot>
-                        </tfoot>
-                    </table>
-                    </div>
+                            </tbody>
+                            <tfoot>
+                            </tfoot>
+                        </table>
                     </div>
                 </div>
+            </div>
             <!-- fin tabla empleados -->
         </main>
     </div>
 </body>
+
 <script>
-            const table = document.getElementById("tabla_empleados");
-const rows = table.getElementsByTagName("tr");
+    $(document).ready(function() {
+        $('#tabla_empleados').DataTable({
 
-for (let z = 1; z < rows.length; z++) {
-    const activoCell = rows[z].cells[4];
-    const estado = activoCell.getAttribute("data-activo");
+            responsive: true,
+            dom: 'Bfrtilp',
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                },
+            },
+            buttons: [{
+                    extend: 'excelHtml5',
+                    text: '<i class="fas fa-file-excel"></i> ',
+                    titleAttr: 'Exportar a Excel',
+                    className: 'btn btn-success'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    text: '<i class="fas fa-file-pdf"></i> ',
+                    titleAttr: 'Exportar a PDF',
+                    className: 'btn btn-danger'
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="fa fa-print"></i> ',
+                    titleAttr: 'Imprimir',
+                    className: 'btn btn-info'
+                },
+            ],
 
-    if (estado === "1") {
-        rows[z].style.backgroundColor = "";
-    } else {
-        rows[z].style.backgroundColor = "lightgray";
+
+            "order": [
+                [0, "desc"]
+            ],
+            'pageLength': 25,
+
+
+        });
+
+    });
+</script>
+<script>
+    const table = document.getElementById("tabla_empleados");
+    const rows = table.getElementsByTagName("tr");
+
+    for (let z = 1; z < rows.length; z++) {
+        const activoCell = rows[z].cells[4];
+        const estado = activoCell.getAttribute("data-activo");
+
+        if (estado === "1") {
+            rows[z].style.backgroundColor = "";
+        } else {
+            rows[z].style.backgroundColor = "lightgray";
+        }
     }
-}
-        </script>
+</script>
+
 </html>
