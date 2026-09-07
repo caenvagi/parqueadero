58e9de61809684607489c81ad1e942e5019a6ab3
Carlos Valencia <caenvagi@hotmail.com>
2026-09-07 08:52:32 -0500

    Actualizacion 	modified:   caja/caja_registrar_procesar.php 	modified:   caja/parqueo_casetas_hora.php 	modified:   logs/head.php 	modified:   parqueo_casetas/parqueo_casetas_hora.php 	modified:   parqueo_hora/dashboard.php 	modified:   parqueo_hora/park-list.php 	modified:   parqueo_hora/parqueo_form.php 	modified:   parqueo_hora/parqueo_listar.php 	modified:   parqueo_hora/parqueo_procesar.php 	modified:   parqueo_hora/parqueo_tarifas.php 	modified:   parqueo_hora/parqueo_tickets.php 	modified:   parqueo_hora/registrar_salida.php 	modified:   parqueo_hora/validar_recibo.php

--- caja/caja_registrar_procesar.php
+++ caja/caja_registrar_procesar.php
@@ -3,6 +3,7 @@ require_once "../conexion/conexion.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fecha = $_POST['fecha_movimiento'];
+    $recibo_id = 0;
     $movimiento = $_POST['movimiento'];
     $desc = $_POST['desc_movimiento'];
     $tipo = $_POST['tipo'];
@@ -33,12 +34,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $pdo->exec("ALTER TABLE caja ADD COLUMN recibo_id VARCHAR(100) NULL");
         }
 
-        $sql = "INSERT INTO caja (fecha_movimiento, movimiento, desc_movimiento, valor_ingreso, valor_egreso, user_login, liquidado, caja_tipo, caja, recibo_id)
-                VALUES (:fecha, :movimiento, :descripcion, :valor_ingreso, :valor_egreso, :user_login, :liquidado, :caja_tipo, :caja, :recibo_id)";
+        $sql = "INSERT INTO caja (fecha_movimiento, recibo_id, movimiento, desc_movimiento, valor_ingreso, valor_egreso, user_login, liquidado, caja_tipo, caja)
+                VALUES (:fecha, :recibo_id, :movimiento, :descripcion, :valor_ingreso, :valor_egreso, :user_login, :liquidado, :caja_tipo, :caja)";
 
         $stmt = $pdo->prepare($sql);
         $stmt->execute([
             ':fecha' => $fecha,
+            ':recibo_id' => $recibo_id,
             ':movimiento' => $movimiento,
             ':descripcion' => $desc,
             ':valor_ingreso' => $valor_ingreso,
@@ -46,8 +48,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             ':user_login' => $user_login,
             ':liquidado' => $liquidado,
             ':caja_tipo' => $caja_tipo,
-            ':caja' => $caja,
-            ':recibo_id' => $reciboValor
+            ':caja' => $caja
+           // ':recibo_id' => $reciboValor
         ]);
 
         echo "OK";
--- logs/head.php
+++ logs/head.php
@@ -44,7 +44,7 @@
         (function() {
             // Tiempo de inactividad en milisegundos (3 minutos - pruebas)
             // Para producción cambiar a 20 * 60 * 1000 (20 minutos)
-            var maxInactive = 3 * 60 * 1000; // 3 minutos
+            var maxInactive = 20 * 60 * 1000; // 20 minutos
 
             var timer;
             var isLoggingOut = false;
